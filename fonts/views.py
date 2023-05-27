7ecf020672c953b5e9591ed8d8cace3b07761573
Sahil <sdhiman92001@gmail.com>
2023-05-27 23:05:53 +0530

    2text

--- fonts/views.py
+++ fonts/views.py
@@ -3,9 +3,7 @@ import random
 import requests
 
 
-
-
-def fontgen(request):
+def fontname():
     url="https://www.googleapis.com/webfonts/v1/webfonts?key=AIzaSyDW2gZ2GC-uIaRHrxu0zVK1tscYGMRaZ1E"
     response = requests.get(url)
 
@@ -17,7 +15,20 @@ def fontgen(request):
         font= random_font["family"]
     else:
         font="Failed to get font"
-    context = {
-        "variable":font
-    }
+    return font
+
+def fontgen(request):
+    font1=fontname()
+    font2=fontname()
+    if font1!="Failed to get font":
+        context = {
+            "variable1":font1,
+            "variable2":font2
+        }
+    else:
+        context = {
+            "variable1":font1,
+            "variable2":""
+        }
+
     return render(request,"fontgen.html",context)
