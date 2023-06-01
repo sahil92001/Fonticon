d7d0733564e5b5d3fc5fdb6246254b769f52a56f
Sahil <sdhiman92001@gmail.com>
2023-06-01 23:41:25 +0530

    improved functions

--- fonts/views.py
+++ fonts/views.py
@@ -3,9 +3,7 @@ import random
 import requests
 
 
-
-
-def fontgen(request):
+def font_var():
     url="https://www.googleapis.com/webfonts/v1/webfonts?key=AIzaSyDW2gZ2GC-uIaRHrxu0zVK1tscYGMRaZ1E"
     response = requests.get(url)
 
@@ -23,25 +21,13 @@ def fontgen(request):
         "download":download
 
     }
+    return context
+
+def fontgen(request):
+    context = font_var()
     return render(request,"fontgen/fontgen.html",context)
 
 def cardgen(request):
-    url="https://www.googleapis.com/webfonts/v1/webfonts?key=AIzaSyDW2gZ2GC-uIaRHrxu0zVK1tscYGMRaZ1E"
-    response = requests.get(url)
-
-    if response.status_code == 200:
-        fonts_data = response.json()
-        fonts = fonts_data.get("items", [])
-        random_font = random.choice(fonts)
-
-        font= random_font["family"]
-        download=random_font['files']['regular']
-    else:
-        font="Failed to get font"
-    context = {
-        "variable":font,
-        "download":download
-
-    }
+    context = font_var()
     return render(request,"fontgen/cardbody.html",context)
 
